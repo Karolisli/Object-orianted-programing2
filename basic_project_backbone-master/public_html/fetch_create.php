ae2ac64af7801db66c5c369d7a6ce86ebce20777
Karolis <karolisliesys@gmail.com>
2019-08-22 10:29:20 +0300

    fetch_create with comments

--- basic_project_backbone-master/public_html/fetch_create.php
+++ basic_project_backbone-master/public_html/fetch_create.php
@@ -1,4 +1,3 @@
-<?php ?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -21,87 +20,100 @@
             <input type="submit">
         </form>
         <div id="drinks-container">
-
+​
         </div>
-        <script>
-            const endpointUrl = "/api/drinks/create.php";
-            const drinksDiv = document.getElementById("drinks-container");
+    <script>
+        const endpointUrl = "api/drinks/create.php";
+        const drinksDiv = document.getElementById("drinks-container");
 
+        document.getElementById("create-form")
+                .addEventListener("submit", e => {
+                    e.preventDefault();
 
+                    // Kad PHP traktuotų requestą kaip POST
+                    // ir normaliai sudėtų į _POST duomenis
+                    // reikia kurti FormData.
+                    let formData = new FormData(e.target);
+                    // Kreipiamės į savo API'jų
+                    fetch(endpointUrl, {
+                        method: "POST",
+                        // Tai yra POST'o duomenys. Atsiminkime brangieji
+                        body: formData
+                    })
+                            // Serverio atsakymas (jau JSONinis)
+                            .then(response => {
+                                // Kadangi responsas pas mus get.php
+                                // yra json encodintas, tai mes naudojam
+                                // nebe .text() o .json().
+                                response.json()
+                                // Tada
+                                        .then(obj => {
+                                            // Tikriname ar iraseme/neiraseme duomenys i faila
+                                            // ir jaigu isiraso tai atvaizduojam tai ka irasome  
+                                            if (obj.status == 'success') {                                      
+                                                displayDrink(obj.data);
+                                                // ŠITAS OBJ yra javascriptinis objektas
+                                                // (išdecodintas jsonas)
+                                                console.log(obj);
+                                            } 
+                                            // Jaigu nepavyksta irasyti i faila atvaizduojam 
+                                            // errora tam laukui kuris priklauso
+                                            else {
+                                                displayErrors(obj.errors);
+                                            }
 
-            document.getElementById("create-form")
-                    .addEventListener("submit", e => {
-                        e.preventDefault();
-
-                        // Kad PHP traktuotų requestą kaip POST
-                        // ir normaliai sudėtų į _POST duomenis
-                        // reikia kurti FormData.
-                        let formData = new FormData(document.querySelector('form'));
-                        // Kreipiamės į savo API'jų
-                        fetch(endpointUrl, {
-                            method: "POST",
-                            // Tai yra POST'o duomenys. Atsiminkime brangieji
-                            body: formData
-                        })
-                                // Serverio atsakymas (jau JSONinis)
-                                .then(response => {
-                                    // Kadangi responsas pas mus get.php
-                                    // yra json encodintas, tai mes naudojam
-                                    // nebe .text() o .json().
-                                    response.json()
-                                            .then(obj => {
-                                                if (obj.status == 'success') {
-                                                  
-                                                    displayDrink(obj.data);
-                                                    // ŠITAS OBJ yra javascriptinis objektas
-                                                    // (išdecodintas jsonas)
-                                                    console.log(obj);
-                                                } else {
-                                                    removeErrors(obj.errors);
-                                                }
-
-                                            });
-                                })
-                                .catch(e => console.log(e.message));
-                    });
-
-            function displayErrors(errors) {
-                console.log(errors);
-                removeErrors();
-                Object.keys(errors).forEach(function (error_id) {
-                    console.log(error_id, errors[error_id]);
-                    const field = document.querySelector('input[name="' + error_id + '"]');
-                    const span = document.createElement("span");
-                    span.className = 'field_error';
-                    span.append(document.createTextNode(errors[error_id]));
-                    field.parentNode.append(span);
-                    console.log(field);
+                                        });
+                            })
+                            .catch(e => console.log(e.message));
+                });
+        // Funkcija kuria iskvietus parodys
+        // tam tikrus fieldu errorus          
+        function displayErrors(errors) {
+            // Paleidziam funkcija kuri istrina error messagus
+            removeErrors();
+            // Kadangi errors yra objektas arrayjuje
+            // sukame cikla per objekta kad gautume erorru raktus (input name)
+            Object.keys(errors).forEach(function (error_id) {
+                // Sukuriam kintamaji kuris paselektina
+                // tam tikra lauka, kuriame bus erroras 
+                const field = document.querySelector('input[name="' + error_id + '"]');
+                // Sukuriam kintamji kuris sukuria elementa span
+                const span = document.createElement("span");
+                // Span kintamajam duodame atributas
+                span.className = 'field_error';
+                // I span kintamaji pridedam jam skirta lauko errora
+                span.innerHTML = errors[error_id];
+                // I field pridedam span kintamaji
+                field.parentNode.append(span);
+            })
+        }
+        // Funkcija kuri istrina visus error'u laukus
+        function removeErrors() {
+            // Pasiselektinam visus laukus su tam priskirtu vardu 
+            const errors = document.querySelectorAll('.field_error');
+            // Tikrinam ar yra errorai ir jaigu yra tikrindami juos per cikla istriname  
+            if (errors) {
+                errors.forEach(function (node) {
+                    node.parentNode.removeChild(node);
                 });
             }
+        }
+        // Funkcija kuri atvaizduos gerima 
+        function displayDrink(value) {
 
-            function removeErrors() {
-                const errors = document.querySelectorAll('.field_error');
-                if (errors) {
-                    Array.prototype.forEach.call(errors, function (node) {
-                        node.parentNode.removeChild(node);
-                    });
-                }
-            }
+            const h2name = document.createElement("h2");
+            const h2abarot = document.createElement("h2");
+            const h2amount = document.createElement("h2");
+            const img = document.createElement("img");
+            img.src = value.image;
+            h2name.innerHTML = value.name;
+            h2abarot.innerHTML = value.abarot;
+            h2amount.innerHTML = value.amount_ml;
 
-            function displayDrink(value) {
-                const h2name = document.createElement("h2");
-                const h2abarot = document.createElement("h2");
-                const h2amount = document.createElement("h2");
-                const img = document.createElement("img");
-                img.src = value.image;
-                h2name.innerHTML = value.name;
-                h2abarot.innerHTML = value.abarot;
-                h2amount.innerHTML = value.amount_ml;
+            drinksDiv.innerHTML = "";
 
-                drinksDiv.innerHTML = "";
-
-                drinksDiv.append(h2name, h2abarot, h2amount, img);
-            }
-        </script>
+            drinksDiv.append(h2name, h2abarot, h2amount, img);
+        }
+    </script>
     </body>
 </html>
