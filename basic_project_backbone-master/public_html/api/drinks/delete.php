ae7f13e3254a30a438d9712d126c5748b455cbc5
Karolis <karolisliesys@gmail.com>
2019-08-22 15:21:02 +0300

    created delete.php

---
+++ basic_project_backbone-master/public_html/api/drinks/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../../../bootloader.php';
+
+$drinksModel = new App\Drinks\Model();
+$drinks = $drinksModel->get(['id' => intval($_POST['id'])]);
+if (!$drinks) {
+    print json_encode([
+        'status' => 'fail',
+        'errors' => [
+            'Drink doesn`t exist'
+        ]
+    ]);
+} else {
+    $drinksModel->delete($drinks[0]);
+    print json_encode([
+        'status' => 'success',
+        'errors' => []
+    ]);
+}
